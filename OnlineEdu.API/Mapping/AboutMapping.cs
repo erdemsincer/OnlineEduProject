816ddcce9c22d35dc4040697b1b882bb46682c51
Erdem <erdemsincer123@icloud.com>
2024-12-17 01:02:59 +0300

    About Controller Yapıldı

---
+++ OnlineEdu.API/Mapping/AboutMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using OnlineEdu.DTO.Dtos.AboutDto;
+using OnlineEdu.Entity.Entities;
+
+namespace OnlineEdu.API.Mapping
+{
+    public class AboutMapping : Profile
+    {
+        public AboutMapping()
+        {
+            CreateMap<CreateAboutDto,About>().ReverseMap();
+            CreateMap<UpdateAboutDto,About>().ReverseMap();
+        }
+    }
+}
--- OnlineEdu.API/Program.cs
+++ OnlineEdu.API/Program.cs
@@ -1,10 +1,18 @@
 using Microsoft.EntityFrameworkCore;
+using OnlineEdu.Business.Abstract;
+using OnlineEdu.Business.Concrete;
+using OnlineEdu.DataAccess.Abstract;
 using OnlineEdu.DataAccess.Context;
+using OnlineEdu.DataAccess.Repositories;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped(typeof(IGenericService<>), typeof(GenericManager<>));
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
