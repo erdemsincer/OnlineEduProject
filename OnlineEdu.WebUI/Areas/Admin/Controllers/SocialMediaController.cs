4a53d2a805056a9e0c5a61b27c63916ff871e7d6
Erdem <erdemsincer123@icloud.com>
2024-12-21 02:58:20 +0300

    Message Uı

---
+++ OnlineEdu.WebUI/Areas/Admin/Controllers/SocialMediaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineEdu.WebUI.Areas.Admin.Controllers
+{
+    public class SocialMediaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
