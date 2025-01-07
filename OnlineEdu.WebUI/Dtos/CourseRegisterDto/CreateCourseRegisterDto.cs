f56234013366fdeff53a3069962da9ff42d5b838
Erdem <erdemsincer123@icloud.com>
2025-01-08 02:07:09 +0300

    Student Area işlemleri yapıldı

---
+++ OnlineEdu.WebUI/Dtos/CourseRegisterDto/CreateCourseRegisterDto.cs
@@ -0,0 +1,14 @@
+﻿using OnlineEdu.Entity.Entities;
+using OnlineEdu.WebUI.Dtos.CourseDtos;
+
+namespace OnlineEdu.WebUI.Dtos.CourseRegisterDto
+{
+    public class CreateCourseRegisterDto
+    {
+        public int CourseRegisterId { get; set; }
+        public int AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+        public int CourseId { get; set; }
+        public ResultCourseDto Course { get; set; }
+    }
+}
