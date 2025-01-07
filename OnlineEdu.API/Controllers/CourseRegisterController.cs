fd92a4c9be1256068f01d6350f5bb9fbdcb14b3b
Erdem <erdemsincer123@icloud.com>
2025-01-07 17:19:58 +0300

    Course Register Endpointleri Hazırlandı

---
+++ OnlineEdu.API/Controllers/CourseRegisterController.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.Business.Abstract;
+using OnlineEdu.DTO.Dtos.CourseRegisterDto;
+using OnlineEdu.Entity.Entities;
+
+namespace OnlineEdu.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseRegisterController(IGenericService<CourseRegister> _courseRegisterService, IMapper _mapper) : ControllerBase
+    {
+        [HttpGet("GetMyCourses/{userId}")]
+        public IActionResult GetMyCourses(int userId)
+        {
+            var values = _courseRegisterService.TGetFilteredList(x => x.AppUserId == userId);
+            var mappedValues = _mapper.Map<List<ResultCourseRegisterDto>>(values);
+            return Ok(mappedValues);
+        }
+
+        [HttpPost]
+
+        public IActionResult CreateCourse(CreateCourseRegisterDto createCourseRegisterDto)
+        {
+            var newValue = _mapper.Map<CourseRegister>(createCourseRegisterDto);
+            _courseRegisterService.TCreate(newValue);
+            return Ok("Kursa Kayıt başarılı");
+        }
+        [HttpPut]
+
+        public IActionResult UpdateCourse(UpdateCourseRegisterDto updateCourseRegisterDto)
+        {
+            var value = _mapper.Map<CourseRegister>(updateCourseRegisterDto);
+            _courseRegisterService.TUpdate(value);
+            return Ok("gÜNCELLENDİ");
+
+        }
+        [HttpDelete]
+        public IActionResult DeleteCourse(int id)
+        {
+            _courseRegisterService.TDelete(id);
+            return Ok("sİLİNDİ");
+        }
+        [HttpGet("{id}")]
+
+        public IActionResult GetById(int id) {
+          var value=_courseRegisterService.TGetById(id);
+          var mappedValue=_mapper.Map<CourseRegister>(value);
+            return Ok(mappedValue);
+        }
+    }
+}
