d86fb51c163fb3f5d6b4f9a2633be767ba42d74e
Erdem <erdemsincer123@icloud.com>
2025-01-27 18:56:10 +0300

    Son Bloglar ve Abone Olma

--- OnlineEdu.WebUI/Controllers/BlogController.cs
+++ OnlineEdu.WebUI/Controllers/BlogController.cs
@@ -1,15 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OnlineEdu.WebUI.Dtos.BlogDtos;
+using OnlineEdu.WebUI.Dtos.SubscriberDtos;
 using OnlineEdu.WebUI.Helpers;
 
 namespace OnlineEdu.WebUI.Controllers
 {
     public class BlogController : Controller
     {
-       
+        private readonly HttpClient _client=HttpClientInstance.CreateClient();
+
         public   IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Subscribe(CreateSubscriberDto createSubscriberDto)
+        {
+            await _client.PostAsJsonAsync("subscribers", createSubscriberDto);
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ OnlineEdu.WebUI/ViewComponents/Blog/_BlogRecentBlogs.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.WebUI.Dtos.BlogDtos;
+using OnlineEdu.WebUI.Helpers;
+
+namespace OnlineEdu.WebUI.ViewComponents.Blog
+{
+    public class _BlogRecentBlogs:ViewComponent
+    {
+        private readonly HttpClient _httpClient = HttpClientInstance.CreateClient();
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var values = await _httpClient.GetFromJsonAsync<List<ResultBlogDto>>("blogs/GetLast4Blogs");
+        return View(values);
+    }
+}
+}
