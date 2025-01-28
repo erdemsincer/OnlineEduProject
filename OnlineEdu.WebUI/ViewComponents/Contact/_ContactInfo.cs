96ed6ffed8689efcba028c5826d1d807094b3bf8
Erdem <erdemsincer123@icloud.com>
2025-01-28 15:18:01 +0300

    İletişim bilgileri

---
+++ OnlineEdu.WebUI/ViewComponents/Contact/_ContactInfo.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.WebUI.Dtos.ContactDtos;
+using OnlineEdu.WebUI.Helpers;
+
+namespace OnlineEdu.WebUI.ViewComponents.Contact
+{
+    public class _ContactInfo:ViewComponent
+    {
+        private readonly HttpClient _client= HttpClientInstance.CreateClient();
+
+
+
+        public async Task< IViewComponentResult> InvokeAsync()
+        {
+            var values = await _client.GetFromJsonAsync<List<ResultContactDto>>("contacts");
+            return View(values);
+        }
+    }
+}
