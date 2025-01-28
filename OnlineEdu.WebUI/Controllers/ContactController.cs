17e244bb887b9647acf0a5b0d51495e42d9e9eb9
Erdem <erdemsincer123@icloud.com>
2025-01-28 15:05:30 +0300

    Mesaj Gönderme işlemi

---
+++ OnlineEdu.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.WebUI.Dtos.ContactDtos;
+using OnlineEdu.WebUI.Dtos.MessageDtos;
+using OnlineEdu.WebUI.Helpers;
+
+namespace OnlineEdu.WebUI.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+        public   IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+
+        public async Task<IActionResult> SendMessage(CreateMessageDto createMessageDto)
+        {
+            await _client.PostAsJsonAsync("messages", createMessageDto);
+            return NoContent();
+        }
+    }
+}
---
+++ OnlineEdu.WebUI/ViewComponents/Contact/_ContactSendMessage.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.WebUI.Helpers;
+
+namespace OnlineEdu.WebUI.ViewComponents.Contact
+{
+    public class _ContactSendMessage:ViewComponent
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+        public  IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
